6cd9fd5a8107b2dff4dffc60e01e2cfcfabd12bb
mariko-33 <61106156+mariko-33@users.noreply.github.com>
2020-10-30 22:10:12 +0800

    upload code

---
+++ mariko-33_hmi_dll/mariko-33_hmi.cpp
@@ -0,0 +1,311 @@
+
+
+#include "mariko-33_hmi.h"
+
+using std::cout;
+using std::endl;
+
+#ifndef NAME_LEN 
+#def NAME_LEN 256
+#endif
+
+
+
+HHOOK _hook;
+
+
+HWND dhand = GetDesktopWindow();
+HMONITOR mhand = MonitorFromWindow(dhand, MONITOR_DEFAULTTONEAREST);
+
+//DEVICE_SCALE_FACTOR pScale;
+
+//POINT pt;
+
+//pt.x = pt.y = 1;
+
+//HMONITOR mhand = MonitorFromPoint(pt, MONITOR_DEFAULTTOPRIMARY);
+
+//HRESULT SCAL_OK = GetScaleFactorForMonitor(mhand, &pScale);//get scale
+
+
+
+HWND g_hwnd = NULL, curr = NULL;
+
+char curr_title[NAME_LEN];
+
+MOUSEHOOKSTRUCT* pMouseS = NULL;
+
+POINT lopot;
+
+float scal_f = 1, sorn;
+
+float get_scal() {
+
+    MONITORINFOEX miex;
+    miex.cbSize = sizeof(miex);
+    GetMonitorInfo(mhand, &miex);
+    int cxLogical = (miex.rcMonitor.right - miex.rcMonitor.left);
+    int cyLogical = (miex.rcMonitor.bottom - miex.rcMonitor.top);
+    //get logical
+    
+    DEVMODE dm;
+    dm.dmSize = sizeof(dm);
+    dm.dmDriverExtra = 0;
+    EnumDisplaySettings(miex.szDevice, ENUM_CURRENT_SETTINGS, &dm);
+    int cxPhysical = dm.dmPelsWidth;
+    int cyPhysical = dm.dmPelsHeight;
+    //get physical
+
+    
+    float horzScale = ((float)cxPhysical / (float)cxLogical);
+    float vertScale = ((float)cyPhysical / (float)cyLogical);
+    //calculate
+
+    sorn = vertScale;
+    float scl = 1 / vertScale;
+
+    return scl;
+}
+
+LRESULT CALLBACK HookCallback(int code , WPARAM wparam , LPARAM lparam){
+
+    pMouseS = (MOUSEHOOKSTRUCT*)lparam;
+    if(code == 0){
+
+            if(pMouseS != NULL){
+
+                //lopot.x = pMouseS->pt.x / 2;
+                //lopot.y = pMouseS->pt.y / 2;
+
+                //curr = WindowFromPoint(lopot);
+
+                //GetWindowTextA(curr, curr_title, sizeof(curr_title));
+
+
+                /*
+                if(!strcmp(curr_title, "FolderView")){
+                    switch(wparam){
+                        case WM_LBUTTONDOWN:{
+                            PostMessageW(g_hwnd, WM_LBUTTONDOWN, NULL, MAKELPARAM(pMouseS->pt.x / 2, pMouseS->pt.y / 2));
+                            break;
+                        }
+
+                        case WM_LBUTTONUP:{
+                            PostMessageW(g_hwnd, WM_LBUTTONUP, NULL, MAKELPARAM(pMouseS->pt.x / 2, pMouseS->pt.y / 2));
+                            break;
+                        }
+                        default:break;
+                    }
+                }/////////////*/
+                
+                switch (wparam) {
+                case WM_LBUTTONDOWN: {
+
+                    curr = WindowFromPoint(pMouseS->pt);
+
+                    GetWindowTextA(curr, curr_title, sizeof(curr_title));
+
+                    if (!strcmp(curr_title, "FolderView")) {
+                        //lopot.x = pMouseS->pt.x / 2;
+                        //lopot.y = pMouseS->pt.y / 2;
+
+                        //curr = WindowFromPoint(pMouseS->pt);
+
+                        //GetWindowTextA(curr, curr_title, sizeof(curr_title));
+
+
+                        //SetForegroundWindow(g_hwnd);
+                        PostMessageW(g_hwnd, WM_LBUTTONDOWN, NULL, MAKELPARAM(pMouseS->pt.x, pMouseS->pt.y));
+                    }
+                    else {
+                        break;/// !!!!!!!!!!!!!!!!!
+                    }
+
+                    break;
+                }
+                case WM_LBUTTONUP: {
+
+                    curr = WindowFromPoint(pMouseS->pt);
+
+                    GetWindowTextA(curr, curr_title, sizeof(curr_title));
+
+                    if (!strcmp(curr_title, "FolderView")) {
+                        //lopot.x = pMouseS->pt.x / 2;
+                        //lopot.y = pMouseS->pt.y / 2;
+
+
+                        //SetForegroundWindow(g_hwnd);
+                        PostMessageW(g_hwnd, WM_LBUTTONUP, NULL, MAKELPARAM(pMouseS->pt.x, pMouseS->pt.y));
+                    }
+                    else {
+                        break;/// !!!!!!!!!!!!!!!!!
+                    }
+
+
+                    break;
+                }
+
+                default:break;
+
+                }
+                
+
+            }
+
+    }
+
+    return CallNextHookEx(_hook, code, wparam, lparam);
+
+}
+
+LRESULT CALLBACK HookCallback_S(int code, WPARAM wparam, LPARAM lparam) {
+
+    pMouseS = (MOUSEHOOKSTRUCT*)lparam;
+    if (code == 0) {
+
+        if (pMouseS != NULL) {
+
+            //lopot.x = pMouseS->pt.x / 2;
+            //lopot.y = pMouseS->pt.y / 2;
+
+            //curr = WindowFromPoint(lopot);
+
+            //GetWindowTextA(curr, curr_title, sizeof(curr_title));
+
+
+            /*
+            if(!strcmp(curr_title, "FolderView")){
+                switch(wparam){
+                    case WM_LBUTTONDOWN:{
+                        PostMessageW(g_hwnd, WM_LBUTTONDOWN, NULL, MAKELPARAM(pMouseS->pt.x / 2, pMouseS->pt.y / 2));
+                        break;
+                    }
+
+                    case WM_LBUTTONUP:{
+                        PostMessageW(g_hwnd, WM_LBUTTONUP, NULL, MAKELPARAM(pMouseS->pt.x / 2, pMouseS->pt.y / 2));
+                        break;
+                    }
+                    default:break;
+                }
+            }/////////////*/
+
+            
+            switch (wparam) {
+            case WM_LBUTTONDOWN: {
+
+                lopot.x = scal_f * pMouseS->pt.x;
+                lopot.y = scal_f * pMouseS->pt.y;
+                curr = WindowFromPoint(lopot);
+                GetWindowTextA(curr, curr_title, sizeof(curr_title));
+                if (!strcmp(curr_title, "FolderView")) {
+                    //lopot.x = pMouseS->pt.x / 2;
+                    //lopot.y = pMouseS->pt.y / 2;
+
+                    //curr = WindowFromPoint(lopot);
+
+                    //GetWindowTextA(curr, curr_title, sizeof(curr_title));
+
+
+                    //SetForegroundWindow(g_hwnd);
+                    PostMessageW(g_hwnd, WM_LBUTTONDOWN, NULL, MAKELPARAM(lopot.x, lopot.y));
+                }
+                else {
+                    break;/// !!!!!!!!!!!!!!!!!
+                }
+
+                break;
+            }
+            case WM_LBUTTONUP: {
+                lopot.x = scal_f * pMouseS->pt.x;
+                lopot.y = scal_f * pMouseS->pt.y;
+                curr = WindowFromPoint(lopot);
+                GetWindowTextA(curr, curr_title, sizeof(curr_title));
+                if (!strcmp(curr_title, "FolderView")) {
+                    //lopot.x = pMouseS->pt.x / 2;
+                    //lopot.y = pMouseS->pt.y / 2;
+
+                    //curr = WindowFromPoint(lopot);
+
+                    //GetWindowTextA(curr, curr_title, sizeof(curr_title));
+
+
+                    //SetForegroundWindow(g_hwnd);
+                    PostMessageW(g_hwnd, WM_LBUTTONUP, NULL, MAKELPARAM(lopot.x, lopot.y));
+                }
+                else {
+                    break;/// !!!!!!!!!!!!!!!!!
+                }
+
+
+                break;
+            }
+
+            default:break;
+
+            }
+            
+
+        }
+
+    }
+
+    return CallNextHookEx(_hook, code, wparam, lparam);
+
+}
+
+unsigned set_hook(wchar_t *wname){
+
+    g_hwnd = FindWindowW(NULL , wname);
+    
+    if(g_hwnd == NULL){
+
+        return 1;//error : unable to find window!
+
+    }
+
+    scal_f = get_scal();
+
+    if (sorn < 1.02) {
+        if (!(_hook = SetWindowsHookEx(WH_MOUSE_LL, HookCallback, NULL, 0))) {
+
+            return 2;//error : unable to set hook!
+
+        }
+    }
+    else {
+        if (!(_hook = SetWindowsHookEx(WH_MOUSE_LL, HookCallback_S, NULL, 0))) {
+
+            return 2;//error : unable to set hook!
+
+        }
+
+    }
+
+    
+
+
+    //if(!(_hook = SetWindowsHookEx(WH_MOUSE_LL , HookCallback, NULL, 0))){
+
+    //        return 2;//error : unable to set hook!
+
+    //}
+
+
+    return 3;//success
+        
+
+
+}
+
+unsigned unhook(){
+
+    if(UnhookWindowsHookEx(_hook)){
+		return 1;
+    }
+    else {
+        exit(-1);
+    }
+	
+	return 0;
+
+}
---
+++ mariko-33_main/predef.h
@@ -0,0 +1,165 @@
+﻿// wxWidgets "Hello World" Program
+// For compilers that support precompilation, includes "wx/wx.h".
+//#include<iostream>
+//#pragma once
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#define WXUSINGDLL
+
+#ifndef UNICODE
+#define UNICODE
+#endif
+
+#ifndef _UNICODE
+#define _UNICODE
+#endif
+
+//#include <string>
+
+#include <tchar.h>
+#include <windows.h>
+#include <process.h>
+#include <Tlhelp32.h>
+#include <winbase.h>
+#include <string.h>
+
+#include <locale.h>
+#include <wx/stattext.h>
+#include <wx/statline.h>
+
+
+#include <wx/wxprec.h>
+#include <wx/choicdlg.h>
+#ifndef WX_PRECOMP
+#include <wx/wx.h>
+#endif
+//#include "mondrian.xpm"
+
+#define wxID_EFFECT 6480
+#define wxID_ENGLISH 6481
+#define wxID_CHINESE 6482
+
+HINSTANCE hdll1 = LoadLibraryW(L"mariko-33_gsw.dll");
+HINSTANCE hdll2 = LoadLibraryW(L"mariko-33_sbw.dll");
+HINSTANCE hdll3 = LoadLibraryW(L"mariko-33_hmi.dll");
+
+
+//hdll = LoadLibraryW(L"mariko-33_gsw.dll");
+
+typedef unsigned(*NUMFUNC)();
+typedef unsigned(*WNAME)(unsigned wnum, wchar_t** name);
+typedef unsigned(*SETWALL)(wchar_t* nam);
+typedef unsigned(*SETHOOK)(wchar_t* wname);
+typedef unsigned(*UNHOOK)();
+typedef unsigned(*PULLOUT)(HWND wnd);
+NUMFUNC get_num = (NUMFUNC)(GetProcAddress(hdll1, "get_windows_num"));
+WNAME get_window = (WNAME)(GetProcAddress(hdll1, "get_all_windows_name"));
+SETWALL set_wall = (SETWALL)(GetProcAddress(hdll2, "set_wall"));
+SETHOOK set_hook = (SETHOOK)(GetProcAddress(hdll3, "set_hook"));
+UNHOOK unhook = (UNHOOK)(GetProcAddress(hdll3, "unhook"));
+PULLOUT pullout = (PULLOUT)(GetProcAddress(hdll2, "pull_out_window"));
+//std::cout << get_num() << std::endl;
+
+//class Staticline : public wxDialog
+//{
+//public:
+//	Staticline(const wxString& title);
+//};
+wchar_t snow_name[]={_T("Snow_galback.exe")};
+
+void killProcessByName(wchar_t *filename)
+{
+    HANDLE hSnapShot = CreateToolhelp32Snapshot(TH32CS_SNAPALL, NULL);
+    PROCESSENTRY32 pEntry;
+    pEntry.dwSize = sizeof(pEntry);
+    BOOL hRes = Process32First(hSnapShot, &pEntry);
+    while (hRes)
+    {
+        if (_tcscmp(pEntry.szExeFile, filename) == 0)
+        {
+            HANDLE hProcess = OpenProcess(PROCESS_TERMINATE, 0,
+                                          (DWORD) pEntry.th32ProcessID);
+            if (hProcess != NULL)
+            {
+                TerminateProcess(hProcess, 9);
+                CloseHandle(hProcess);
+            }
+        }
+        hRes = Process32Next(hSnapShot, &pEntry);
+    }
+    CloseHandle(hSnapShot);
+}
+
+HWND set_or_not = NULL;
+//DWORD pid;
+BOOL config_language(wxLocale* locale);
+
+char lan='1';
+
+class MyApp :public wxApp {
+public:
+	///int yy = 9;
+	virtual bool OnInit();
+	virtual int OnExit();
+	wxLocale locale;
+};
+
+class MyFrame :public wxFrame {
+public:
+	MyFrame(const wxString& title);
+
+
+
+
+	void OnOpen(wxCommandEvent& event);
+	void OnQuit(wxCommandEvent& event);
+	void OnAbout(wxCommandEvent& event);
+	void OnHelp(wxCommandEvent& event);
+	void OnClose(wxCommandEvent& event);
+	void OnEffect(wxCommandEvent& event);
+	void OnClear(wxCommandEvent& event);
+	void OnChinese(wxCommandEvent& event);
+	void OnEnglish(wxCommandEvent& event);
+
+
+	void logtext(wxString infoma);
+
+
+
+	//int yy = 9;
+
+
+	wxString info_to_append = L"test!\n";
+
+	wxString  text = wxT("directx/3d games and some old games may not be supported!\n\n");
+	//		text2 = wxT("eee");
+	//Staticline(const wxString& title);
+
+
+
+
+	wxTextCtrl* info = new wxTextCtrl(this, 1, this->text, wxPoint(0, 0), wxSize(470, 200), wxTE_READONLY | wxTE_MULTILINE);// , text2);
+
+private:
+
+	DECLARE_EVENT_TABLE();
+};
+
+BEGIN_EVENT_TABLE(MyFrame, wxFrame)
+EVT_MENU(wxID_OPEN, MyFrame::OnOpen)
+EVT_MENU(wxID_ABOUT, MyFrame::OnAbout)
+EVT_MENU(wxID_EXIT, MyFrame::OnQuit)
+EVT_MENU(wxID_HELP, MyFrame::OnHelp)
+EVT_MENU(wxID_CLOSE, MyFrame::OnClose)
+EVT_MENU(wxID_CHINESE, MyFrame::OnChinese)
+EVT_MENU(wxID_ENGLISH, MyFrame::OnEnglish)
+EVT_BUTTON(wxID_OPEN, MyFrame::OnOpen)
+EVT_BUTTON(wxID_EXIT, MyFrame::OnQuit)
+EVT_BUTTON(wxID_HELP, MyFrame::OnHelp)
+EVT_BUTTON(wxID_ABOUT, MyFrame::OnAbout)
+EVT_BUTTON(wxID_CLOSE, MyFrame::OnClose)
+EVT_BUTTON(wxID_EFFECT, MyFrame::OnEffect)
+EVT_BUTTON(wxID_CLEAR, MyFrame::OnClear)
+END_EVENT_TABLE();
+
